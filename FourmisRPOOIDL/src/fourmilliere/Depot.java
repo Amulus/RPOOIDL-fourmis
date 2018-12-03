8986ed2b8b70c25b4e1191ab0b433a723346ccf5
Axel Murat <axel.murat@laposte.net>
2018-12-03 20:03:21 +0100

    ?

---
+++ FourmisRPOOIDL/src/fourmilliere/Depot.java
@@ -0,0 +1,28 @@
+package fourmilliere;
+
+import evolution.Adulte;
+import evolution.Larve;
+
+public class Depot {
+	Fourmilliere fourmilliere=null;
+	private int NombreDeMorts=0;
+	public Depot(Fourmilliere fourmilliere){
+		this.fourmilliere=fourmilliere;
+	}
+	public void AjouterMort(Adulte mort){
+		this.fourmilliere.getMorts().remove(mort.getIdentifiant());
+		this.NombreDeMorts++;
+	}
+	public void AjouterMort(Larve mort){
+		this.fourmilliere.getMorts().remove(mort.getIdentifiant());
+		this.NombreDeMorts++;
+	}
+	//Predateur A ajouter
+	
+	public void ViderDepot(){
+		NombreDeMorts=0;
+	}
+	public long getNombreDeMorts(){
+		return this.NombreDeMorts;
+	}
+}
--- FourmisRPOOIDL/src/fourmilliere/Fourmilliere.java
+++ FourmisRPOOIDL/src/fourmilliere/Fourmilliere.java
@@ -2,25 +2,34 @@ package fourmilliere;
 
 import java.util.Hashtable;
 
+import evolution.Adulte;
+import evolution.Larve;
+import evolution.Nymphe;
+import evolution.Oeuf;
 import outils.LireParametres;
 
 public class Fourmilliere {
 
 	private Hashtable<Integer,Adulte> fourmis = new Hashtable<Integer,Adulte>();
+	private Hashtable<Integer,Object> morts = new Hashtable<Integer,Object>();
 	
 	private Nid nid;
+	private Depot Depot;
+	private Reserve Reserve;
 	
 	private LireParametres lectureFichier = new LireParametres();
 
 	public Fourmilliere() {
 		this.fourmis = new Hashtable<Integer,Adulte>();
-		this.nid = new Nid();
+		this.nid = new Nid(this);
+		this.Depot = new Depot(this);
+		this.Reserve = new Reserve(this);
 		
-		Adulte fourmi = new Adulte(this,0);
-		FourmiReine reine = new FourmiReine(fourmi);
+		Adulte Reine = new Adulte(this,0);
+		Reine.setReine();
 		Nymphe nymphe = new Nymphe(this,1);
 		
-		this.fourmis.put(fourmi.getIdentifiant(), fourmi);
+		this.fourmis.put(Reine.getIdentifiant(), Reine);
 		this.nid.getNymphes().put(nymphe.getIdentifiant(),nymphe);
 	}
 
@@ -32,7 +41,9 @@ public class Fourmilliere {
 		return this.lectureFichier;
 	}
 	
-	
+	public Hashtable<Integer,Object> getMorts() {
+		return this.morts;
+	}
 	
 	public Hashtable<Integer, Adulte> getFourmis() {
 		return fourmis;
@@ -50,24 +61,18 @@ public class Fourmilliere {
 		return nid.getLarves();
 	}
 
-	public int nombreDeLarve() {
-		return this.getLarves().size();
-	}
-	
 	public int nombreDeFourmis() {
 		return this.getFourmis().size();
 	}
-	
-	public int nombreDeNymphe() {
-		return this.getNymphes().size();
+	public Depot getDepot(){
+		return this.Depot;
 	}
-	
-	public int nombreOeuf() {
-		return this.getOeufs().size();
+
+	public Reserve getReserve(){
+		return this.Reserve;
 	}
-	
 	public void ajoutOeuf(Oeuf oeuf) {
-		this.nid.ajoutOeuf(oeuf);;
+		this.nid.ajoutOeuf(oeuf);
 	}
 	
 	public void ajoutLarve(Larve larve) {
@@ -77,4 +82,27 @@ public class Fourmilliere {
 	public void ajoutNymphe(Nymphe nymphe) {
 		this.nid.ajoutNymphe(nymphe);
 	}
+	
+	public void affichage(){
+		System.out.println("Nombre d'ouefs: "+this.nid.nombreOeuf());
+		System.out.println("Nombre de larves: "+this.nid.nombreDeLarve());
+		System.out.println("Nombre de Nymphes: "+this.nid.nombreDeNymphe());
+		System.out.println("Nombre de Fourmies: "+this.nombreDeFourmis());
+		int NombreOuvrieres = 0,NombreSoldats = 0,NombreReproducteurs = 0,NombreReine=0;
+		for(int i=0; i< this.fourmis.size();i++){
+			if(this.fourmis.get(i).getNumeroRole()==0){NombreReine++; break;}	
+			if(this.fourmis.get(i).getNumeroRole()==1){NombreOuvrieres++; break;}
+			if(this.fourmis.get(i).getNumeroRole()==2){NombreSoldats++; break;}
+			if(this.fourmis.get(i).getNumeroRole()==3){NombreReproducteurs++; break;}	
+		}
+		System.out.println("Nombre de Fourmies Reine: "+NombreReine);
+		System.out.println("Nombre de Fourmies Ouvrieres: "+NombreOuvrieres);
+		System.out.println("Nombre de Fourmies Soldats: "+NombreSoldats);
+		System.out.println("Nombre de Fourmies Reproducteurs: "+NombreReproducteurs);
+		System.out.println("Nourriture dans la Reserve: "+this.Reserve.getReserveNouriture());
+		System.out.println("Nombre de Morts: "+this.Depot.getNombreDeMorts());
+		
+	}
+
+
 }
