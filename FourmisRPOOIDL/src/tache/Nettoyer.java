2ee6c274f0402386d1003fb891f3098cebf128ee
pierrebrunel <pierrebrunel@172.28.197.253>
2018-12-04 10:38:42 +0100

    Fix test Nettoyer, ajout config, update Todolist

--- FourmisRPOOIDL/src/tache/Nettoyer.java
+++ FourmisRPOOIDL/src/tache/Nettoyer.java
@@ -4,23 +4,31 @@ import evolution.Fourmi;
 import evolution.Larve;
 import fourmilliere.Depot;
 import fourmilliere.Fourmilliere;
+import outils.LireParametres;
 
 //Parcourir l'ensemble de larve et fourmi : check décédé et déplacer dans le dépot
 
 public class Nettoyer extends Tache {
-	Depot depot =null;
-	Fourmilliere fourmilliere=null;
+	private Depot depot =null;
+	private Fourmilliere fourmilliere=null;
+	private int NombreDeMorts=0;
 	public Nettoyer(Depot depot,Fourmilliere fourmilliere){
 		this.depot=depot;
 		this.fourmilliere=fourmilliere;
+		LireParametres lecturefichier = fourmilliere.getLireParametres();
+		this.NombreDeMorts= (int) lecturefichier.ChercherParametre("NombreCadavreMaxNettoyer");
 	}
 	@Override
 	public void execute(Fourmi fourmi) {
-		int i=1;
+		Integer temporaire[] = new Integer[NombreDeMorts];
+		int j=0;
 		for(int cle : this.fourmilliere.getMorts().keySet()){
-			if(i>3) break;
-			i++;
-			Object mort = this.fourmilliere.getMorts().get(cle);
+			if(j>=NombreDeMorts) break;
+			temporaire[j]= cle;
+			j++;
+		}
+		for(int i=0; i<temporaire.length ; i++){
+			Object mort = this.fourmilliere.getMorts().get(temporaire[i]);
 			if(mort.getClass()==Larve.class)
 				this.depot.AjouterMort((Larve)mort);
 			else
